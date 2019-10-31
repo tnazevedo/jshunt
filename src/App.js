75580c9c7b1526d517c59c51ea3cc35b49ad64f0
Talita Azevedo <talita.azevedo360@gmail.com>
2019-10-30 23:35:25 -0300

    projeto JSHunt- semi pronto consumindo api do heroku => pendente configuraçao da rota de produtos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import api from './services/api';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header';
+import Main from './pages/main';
+import './styles.css';
+const App = () =>(
+  <div className="App"> 
+    <Header/>
+    <Routes></Routes>
+  </div>
+);
 
 export default App;
